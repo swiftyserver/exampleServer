5406c7255645d274a75b0a339ce7dfedb154635c
Mehmet Emin KARTAL <kartalbombe@icloud.com>
2019-12-31 00:34:05 -0500

    Docker stuff

--- Sources/exampleServer/main.swift
+++ Sources/exampleServer/main.swift
@@ -1,1 +1,25 @@
-print("Hello, world!")
+
+import SwiftyServer
+
+struct MyEnviroment: HTTPEnviroment {
+	var httpParameters: HTTPRequest
+
+	static var Server: Server<MyEnviroment> = .init()
+}
+
+
+
+MyEnviroment.Server.configure {
+
+	MyEnviroment.get("/")
+		.constant("Hello World")
+		.response()
+
+	MyEnviroment.get("/hello")
+		.constant("Hello")
+		.map { up, req in "\(up) World"}
+		.response()
+
+}.serve()
+
+// curl 127.0.0.1:8080/ to check if your server is working!
